874cefd5cb92580eb61115b87ff0baccfa8108cc
Jahna sri aneela ankam <jahnasrianeelaankam@Jahnas-MacBook-Air.local>
2025-11-20 22:25:30 -0500

    Inital commit with bare bones for ToDoList

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import {ToDoWrapper} from './components/ToDoWrapper'
+
+function App() {
+  return (
+      <div className="App">
+        <ToDoWrapper />     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ToDoWrapper.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { ToDoForm } from "./ToDoForm";
+import {v4 as uuidv4} from "uuid";
+import { ToDo } from "./ToDo";
+uuidv4();
+
+export const ToDoWrapper = () => {
+    const [todos, setTodos] = useState([])
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo,
+            completed: false, isEditing: false}])
+        console.log(todos)
+    }
+    return(
+        <div className="TodoWrapper">
+            <ToDoForm addTodo={addTodo}/>
+            {todos.map((todo, index) => (
+                <ToDo task = {todo} key={index} />
+            ))}
+            
+        </div>
+
+    );
+}
